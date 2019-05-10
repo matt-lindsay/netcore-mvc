46511faf2caf029365afc02ce2b2a91485fd4ebd
matt-lindsay <matt.lindsay@surreyheath.gov.uk>
2019-05-10 12:14:39 +0100

    Added a database

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
-  public class Move
+  public class Movie
   {
     public int Id { get; set; }
     public string Title { get; set; }
